724601fe83ec1f31456b59b0d040a9c0528d1318
Anıl Can Özgök <71594949+anilozgok@users.noreply.github.com>
2023-02-22 21:03:55 +0300

    + spring boot application annotation added.

--- src/main/java/org/anilcan/PhoneBookApplication.java
+++ src/main/java/org/anilcan/PhoneBookApplication.java
@@ -1,7 +1,11 @@
 package org.anilcan;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PhoneBookApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(PhoneBookApplication.class, args);
     }
 }

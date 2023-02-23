84ec744a7eb9f267d1a8508ffec12cf968045af4
Anıl Can Özgök <71594949+anilozgok@users.noreply.github.com>
2023-02-23 22:08:22 +0300

    + <fix>

--- src/main/java/org/anilcan/service/PhoneBookService.java
+++ src/main/java/org/anilcan/service/PhoneBookService.java
@@ -83,12 +83,12 @@ public class PhoneBookService {
 
         phoneBookRepository.delete(contactToDelete);
     }
+}
 
-    private static class PhoneBookHelper {
-        public boolean isPhoneNumberValid(String phoneNumber) {
-            Pattern pattern = Pattern.compile("^(05)([0-9]{2})\\s?([0-9]{3})\\s?([0-9]{2})\\s?([0-9]{2})$");
-            Matcher matcher = pattern.matcher(phoneNumber);
-            return matcher.matches();
-        }
+class PhoneBookHelper {
+    public boolean isPhoneNumberValid(String phoneNumber) {
+        Pattern pattern = Pattern.compile("^(05)([0-9]{2})\\s?([0-9]{3})\\s?([0-9]{2})\\s?([0-9]{2})$");
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
     }
 }

a11d5169e6906942b85d52429bf28dda7a48b083
Anıl Can Özgök <71594949+anilozgok@users.noreply.github.com>
2023-02-23 23:40:52 +0300

    + update on test.

--- src/test/java/org/anilcan/repository/PhoneBookRepositoryTest.java
+++ src/test/java/org/anilcan/repository/PhoneBookRepositoryTest.java
@@ -3,14 +3,8 @@ package org.anilcan.repository;
 import org.anilcan.model.entity.Contact;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 class PhoneBookRepositoryTest {
